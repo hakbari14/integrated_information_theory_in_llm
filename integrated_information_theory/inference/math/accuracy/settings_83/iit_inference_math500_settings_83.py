7dcd6aa77d193f18237fc5f7b08e59c6d3c29adf
hakbari14 <hakbari@email.com>
2026-08-24 03:25:40 +0000

    settings 83

---
+++ integrated_information_theory/inference/math/accuracy/settings_83/iit_inference_math500_settings_83.py
@@ -0,0 +1,43 @@
+from integrated_information_theory.inference.integrated_information_inference import integrated_information_inference
+from integrated_information_theory.datasets.math.math_500_dataset import math_500_dataset
+from integrated_information_theory.datasets.dataset_config import dataset_config
+from integrated_information_theory.logger.inference.accuracy.inference_accuracy_logger import inference_accuracy_logger
+
+class iit_inference_math500_settings_83(integrated_information_inference): 
+
+    def __init__(self, model_name, peft_checkpoint_path=None):
+        super().__init__(model_name, peft_checkpoint_path)
+
+    def get_dataset(self):
+        if self.dataset is None:
+            config = dataset_config(self.model_name)
+            self.dataset = math_500_dataset(config)
+        return self.dataset
+
+    
+    def get_iit_calculator(self):
+        return None
+
+    def get_max_new_tokens(self):
+        return 25000
+
+    def get_logger(self, run_number = 0):
+        if self.logger is None:
+            self.logger = inference_accuracy_logger(log_file_name = f'integrated_information_theory/inference/math/accuracy/settings_83/run_{run_number}/settings_83_math500_full.csv')
+
+        return self.logger
+
+
+for run_number in range(1,6):
+    print(f'{'*' * 100}  Run Number {run_number}  {'*' * 100}')
+    t = iit_inference_math500_settings_83('/home/hr_akbari/research/integrated_information_theory_in_llm/live_logs/settings_83/checkpoint-350-HF')
+    t.get_logger(run_number=run_number)
+    t.calculate_accuracy_vllm()
+    print(f'{'*' * 210}')
+
+# for run_number in range(1,6):
+#     print(f'{'*' * 100}  Run Number {run_number}  {'*' * 100}')
+#     t = iit_inference_math500_settings_83('hakbari/deepseek_r1_qwen_7B_iit_intrinsic_information_83')
+#     t.get_logger(run_number=run_number)
+#     t.calculate_entropy(t.get_logger().get_log_file_name())
+#     print(f'{'*' * 210}')
