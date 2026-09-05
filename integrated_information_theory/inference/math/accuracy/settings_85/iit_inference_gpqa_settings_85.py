079aa93b86dee7963cd8db0fcfb6f2a514ba17ec
hakbari14 <hakbari@email.com>
2026-09-05 08:08:02 +0000

    settings 85 logs

---
+++ integrated_information_theory/inference/math/accuracy/settings_85/iit_inference_gpqa_settings_85.py
@@ -0,0 +1,43 @@
+from integrated_information_theory.inference.integrated_information_inference import integrated_information_inference
+from integrated_information_theory.datasets.math.gpqa_dataset import gpqa_dataset
+from integrated_information_theory.datasets.dataset_config import dataset_config
+from integrated_information_theory.logger.inference.accuracy.inference_accuracy_logger import inference_accuracy_logger
+from integrated_information_theory.intrinsic_information import intrinsic_information
+from integrated_information_theory.config.intrinsic_information_config import intrinsic_information_config
+from integrated_information_theory.enums_class import ii_calculation_type_enum, tpm_creation_type_enum, last_layer_computation_type_enum, granularity_enum
+
+
+
+class iit_inference_gpqa_settings_85(integrated_information_inference): 
+
+    def __init__(self, model_name, peft_checkpoint_path=None):
+        super().__init__(model_name, peft_checkpoint_path)
+
+    def get_dataset(self):
+        if self.dataset is None:
+            config = dataset_config(self.model_name)
+            self.dataset = gpqa_dataset(config)
+        return self.dataset
+
+    
+    def get_iit_calculator(self):
+        return None
+
+    def get_max_new_tokens(self):
+        return 15000
+
+    def get_logger(self, run_number = 0):
+        if self.logger is None:
+            self.logger = inference_accuracy_logger(log_file_name = f'integrated_information_theory/inference/math/accuracy/settings_85/run_{run_number}/settings_85_gpqa_full.csv')
+
+        return self.logger
+
+
+
+for run_number in range(1,6):
+    print(f"{'*' * 100}  Run Number {run_number}  {'*' * 100}")
+    t = iit_inference_gpqa_settings_85('hakbari/qwen3-8b_iit_phi_85')
+    t.get_logger(run_number=run_number)
+    t.calculate_accuracy_vllm()
+    print(f"{'*' * 210}")
+
---
+++ integrated_information_theory/inference/math/accuracy/settings_85/iit_inference_math500_settings_85.py
@@ -0,0 +1,37 @@
+from integrated_information_theory.inference.integrated_information_inference import integrated_information_inference
+from integrated_information_theory.datasets.math.math_500_dataset import math_500_dataset
+from integrated_information_theory.datasets.dataset_config import dataset_config
+from integrated_information_theory.logger.inference.accuracy.inference_accuracy_logger import inference_accuracy_logger
+
+class iit_inference_math500_settings_85(integrated_information_inference): 
+
+    def __init__(self, model_name, peft_checkpoint_path=None):
+        super().__init__(model_name, peft_checkpoint_path)
+
+    def get_dataset(self):
+        if self.dataset is None:
+            config = dataset_config(self.model_name)
+            self.dataset = math_500_dataset(config)
+        return self.dataset
+
+    
+    def get_iit_calculator(self):
+        return None
+
+    def get_max_new_tokens(self):
+        return 25000
+
+    def get_logger(self, run_number = 0):
+        if self.logger is None:
+            self.logger = inference_accuracy_logger(log_file_name = f'integrated_information_theory/inference/math/accuracy/settings_85/run_{run_number}/settings_85_math500_full.csv')
+
+        return self.logger
+
+
+for run_number in range(1,6):
+    print(f"{'*' * 100}  Run Number {run_number}  {'*' * 100}")
+    t = iit_inference_math500_settings_85('hakbari/qwen3-8b_iit_phi_85')
+    t.get_logger(run_number=run_number)
+    t.calculate_accuracy_vllm()
+    print(f"{'*' * 210}")
+
