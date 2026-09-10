fda4e0cde4e04eb658c52e35b86a845e22bcd69b
hakbari14 <hakbari@email.com>
2026-09-10 19:58:18 +0000

    consciousness inferene logs

--- integrated_information_theory/inference/consciousness/known_unknowns/iit_inference_known_unknowns_settings_00.py
+++ integrated_information_theory/inference/consciousness/known_unknowns/iit_inference_known_unknowns_settings_00.py
@@ -1,10 +1,10 @@
 from integrated_information_theory.inference.integrated_information_inference import integrated_information_inference
-from integrated_information_theory.datasets.consciousness.self_reflection_dataset import self_reflection_dataset
+from integrated_information_theory.datasets.consciousness.known_unknowns_dataset import known_unknowns_dataset
 from integrated_information_theory.datasets.dataset_config import dataset_config
 from integrated_information_theory.logger.inference.accuracy.inference_accuracy_logger import inference_accuracy_logger
 
 
-class iit_inference_self_reflection_settings_00(integrated_information_inference): 
+class iit_inference_known_unknowns_settings_00(integrated_information_inference): 
 
     def __init__(self, model_name, peft_checkpoint_path=None):
         super().__init__(model_name, peft_checkpoint_path)
@@ -12,7 +12,7 @@ class iit_inference_self_reflection_settings_00(integrated_information_inference
     def get_dataset(self):
         if self.dataset is None:
             config = dataset_config(self.model_name)
-            self.dataset = self_reflection_dataset(config)
+            self.dataset = known_unknowns_dataset(config)
         return self.dataset
 
     
@@ -24,14 +24,14 @@ class iit_inference_self_reflection_settings_00(integrated_information_inference
 
     def get_logger(self, run_number = 0):
         if self.logger is None:
-            self.logger = inference_accuracy_logger(log_file_name = f'integrated_information_theory/inference/consciousness/self_reflection/run_{run_number}/settings_00_self_reflection.csv')
+            self.logger = inference_accuracy_logger(log_file_name = f'integrated_information_theory/inference/consciousness/known_unknowns/run_{run_number}/settings_00_known_unknowns.csv')
 
         return self.logger
 
 
 for run_number in range(1,6):
     print(f"{'*' * 100}  Run Number {run_number}  {'*' * 100}")
-    t = iit_inference_self_reflection_settings_00('Qwen/Qwen3-8B')
+    t = iit_inference_known_unknowns_settings_00('Qwen/Qwen3-8B')
     t.get_logger(run_number=run_number)
     t.calculate_accuracy_consciousness()
     print(f"{'*' * 210}")
