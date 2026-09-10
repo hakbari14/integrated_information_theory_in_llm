192d895fb5cf5f24b1e984a65fbb67901ed7afcf
hakbari14 <hakbari@email.com>
2026-09-10 04:47:35 +0000

    belief inference logs

--- integrated_information_theory/datasets/consciousness/consciousness_dataset.py
+++ integrated_information_theory/datasets/consciousness/consciousness_dataset.py
@@ -30,6 +30,7 @@ class consciousness_dataset(dataset_handler):
             r'(?i)Answer.*?([AB])\b',            
             r'(?i)boxed\s*\{\s*([ABab])\s*\}',
             r'(?i)answer(?:[\s:*()]+|\bis\b|\boption\b)*([ab])',            
+            r'\(?\s*([AB])\s*\)?',            
         ]
 
         for pattern in patterns:
@@ -44,7 +45,7 @@ class consciousness_dataset(dataset_handler):
     def generate_model_prompt(self, x):
         problem_id = x["unique_id"]
 
-        instruction_begin = self.get_instruction_begin()
+        instruction_begin = "Answer the following question with only the most correct option and no extra content.\n"
         instruction_end = "\nAnswer: "
         prompt = instruction_begin + x["question"] + instruction_end
 
@@ -62,11 +63,5 @@ class consciousness_dataset(dataset_handler):
                 "problem_id": problem_id
                 }
 
-    def get_instruction_begin(self):
-        if "qwen3-8b" in self.config.model_name.lower():
-            return "Solve the following question step by step, then output only the most correct option.\n"
-        
-        return "Answer the following question with only the most correct option and no extra content.\n"
-    
     def get_dataset_path(self):
         return None
